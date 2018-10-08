55e176e378798f1f5bad0b7f7e1366fcb0af62fb
Lawrence Smith <lawrence.smith.3@gmail.com>
2018-10-08 11:17:38 -0500

    Removes run-sequence dependency from build process

--- tasks/build.js
+++ tasks/build.js
@@ -7,27 +7,21 @@
 import del from 'del';
 import gulp from 'gulp';
 import notify from './notify';
-import runSequence from 'run-sequence';
 
 function clean() {
     return del(process.env.DIRECTORY_DEST, {
         force: process.env.ENABLE_UNSAFE_CLEAN === 'true',
     });
 }
-
-export default function build(done) {
-    return runSequence(
-        'clean', 'scripts',
-        () => {
-            process.env.watchStarted = true;
-            done();
-            notify.log(
-                'Build complete!',
-                process.env.WATCH === 'true' ? 'Starting watch...' : '',
-                true
-            );
-        }
-    );
-}
-
 gulp.task('clean', clean);
+
+export default gulp.series('clean', 'scripts', done => {
+    process.env.watchStarted = true;
+    notify.log(
+        'Build complete!',
+        process.env.WATCH === 'true' ? 'Starting watch...' : '',
+        true
+    );
+    done();
+});
+
